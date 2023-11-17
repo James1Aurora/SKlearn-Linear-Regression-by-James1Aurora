9f1f989d56c59f50da3a8b97211469f9dbff9199
Akbar Permana <108169462+James1Aurora@users.noreply.github.com>
2023-11-17 08:15:58 +0700

    Add files via upload

---
+++ Sklearn linear Regression by James1Aurora.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+#buat data jumlah kamar
+bedrooms = np.array([1,1,2,2,3,4,4,5,5,5])
+
+#data harga rumah dalam dollar
+house_price = np.array([15000, 18000, 27000, 34000, 50000, 68000, 65000, 81000,85000, 90000])
+
+#menampilkan angka dalam scatterplot
+import matplotlib.pyplot as plt
+%matplotlib inline
+
+plt.scatter(bedrooms, house_price)
+
+
+
+from sklearn.linear_model import LinearRegression
+
+#latih model dengan regression.fit()
+bedrooms = bedrooms.reshape(-1,1)
+linreg = LinearRegression()
+linreg.fit(bedrooms, house_price)
+
+# menampilkan plot hubungan antara jumlah kamar dengan harga rumah
+plt.scatter(bedrooms, house_price)
+plt.plot(bedrooms, linreg.predict(bedrooms))
